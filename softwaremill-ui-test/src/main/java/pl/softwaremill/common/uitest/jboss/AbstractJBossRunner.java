dba77629a712c02f45e63292bc8767b97854befe
U-DL-M4300-1\Lukasz <Lukasz@DL-M4400-1.(none)>
2010-09-24 09:20:25 +0200

    Added OS detecting functionality to run JBoss also on Windows

--- softwaremill-ui-test/src/main/java/pl/softwaremill/common/uitest/jboss/AbstractJBossRunner.java
+++ softwaremill-ui-test/src/main/java/pl/softwaremill/common/uitest/jboss/AbstractJBossRunner.java
@@ -9,86 +9,97 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /**
- *
  * @author maciek
  * @author Pawel Wrzeszcz
  */
 public abstract class AbstractJBossRunner {
 
     private String serverHome;
-	private String configuration;
+    private String configuration;
     private boolean running;
     private int portset;
-    
+
     Process jbossProcess;
 
-	protected final static SysoutLog log = new SysoutLog();
+    protected final static SysoutLog log = new SysoutLog();
 
-	private static final String STARTED_LOG_MESSAGE = "Started in";
+    private static final String STARTED_LOG_MESSAGE = "Started in";
 
-	private boolean deploymentComplete = false;
+    private boolean deploymentComplete = false;
 
-	private static final int MILLISECONDS_IN_MINUTE = 60 * 1000;
+    private static final int MILLISECONDS_IN_MINUTE = 60 * 1000;
 
-	protected abstract ServerPoperties getServerProperties();
+    protected abstract ServerPoperties getServerProperties();
 
-	protected abstract Deployment[] getDeployments();
+    protected abstract Deployment[] getDeployments();
 
     private Process tailProcess;
 
-	@BeforeSuite
+    @BeforeSuite
     public void start() throws Exception {
-		loadProperties();
-		scheduleTimeout();
-		undeploy(); // Clean old deployments
-		startServerIfNeeded();
-		deploy();
+        loadProperties();
+        scheduleTimeout();
+        undeploy(); // Clean old deployments
+        startServerIfNeeded();
+        deploy();
     }
 
-	@AfterSuite(alwaysRun = true)
-	public void shutdown() throws Exception {
-    	undeploy();
+    @AfterSuite(alwaysRun = true)
+    public void shutdown() throws Exception {
+        undeploy();
         if (!running) {
-			log.info("Stopping JBoss server");
-			shutdownServer();
-			log.info("JBoss Server stopped");
+            log.info("Stopping JBoss server");
+            shutdownServer();
+            log.info("JBoss Server stopped");
         }
-		publishLog();
+        publishLog();
     }
 
-	private void loadProperties() {
-		ServerPoperties serverPoperties = getServerProperties();
+    private void loadProperties() {
+        ServerPoperties serverPoperties = getServerProperties();
 
-		this.serverHome = serverPoperties.getServerHome();
-		this.configuration = serverPoperties.getConfiguration();
-		this.running =serverPoperties.isRunning();
-		this.portset = serverPoperties.getPortset();
-	}
+        this.serverHome = serverPoperties.getServerHome();
+        this.configuration = serverPoperties.getConfiguration();
+        this.running = serverPoperties.isRunning();
+        this.portset = serverPoperties.getPortset();
+    }
 
-	private void startServerIfNeeded() throws Exception {
+    private void startServerIfNeeded() throws Exception {
         if (!running) {
-			log.info("Starting JBoss server");
-			startServer();
-			log.info("JBoss started");
-		}
+            log.info("Starting JBoss server");
+            startServer();
+            log.info("JBoss started");
+        }
     }
 
     protected void startServer() throws Exception {
-        jbossProcess = Runtime.getRuntime().exec(new String[]{serverHome + "/bin/run.sh", "-c", configuration,
-                "-Djboss.service.binding.set=ports-0"+portset});
-        
-		waitFor(jbossProcess, STARTED_LOG_MESSAGE);
+        jbossProcess = Runtime.getRuntime().exec(new String[]{serverHome + createRunScript(), "-c", configuration,
+                "-Djboss.service.binding.set=ports-0" + portset});
+
+        waitFor(jbossProcess, STARTED_LOG_MESSAGE);
 
         tailProcess = jbossProcess;
-	}
+    }
 
-	private void waitFor(Process process, String message) {
-		log.info("Waiting for message: [" + message + "]");
-		Scanner scanner = new Scanner(process.getInputStream()).useDelimiter(message);
-		scanner.next();
-	}
+    private String createRunScript() {
+        if (winSystem()) {
+            return "/bin/run.bat";
+        }
+        return "/bin/run.sh";
+    }
 
-	private Process getTailProcess() throws IOException {
+    private boolean winSystem() {
+        String osName = System.getProperty("os.name");
+        return osName != null && osName.indexOf("Windows") != -1;
+    }
+
+    private void waitFor(Process process, String message) {
+        log.info("Waiting for message: [" + message + "]");
+        Scanner scanner = new Scanner(process.getInputStream()).useDelimiter(message);
+        scanner.next();
+    }
+
+    private Process getTailProcess() throws IOException {
         if (tailProcess == null) {
             // this happens when jboss was already started
             tailProcess = Runtime.getRuntime().exec(
@@ -96,76 +107,83 @@ public abstract class AbstractJBossRunner {
         }
 
         return tailProcess;
-	}
+    }
 
-	protected String getServerLogPath() {
-		return serverHome + "/server/" + configuration + "/log/server.log";
-	}
+    protected String getServerLogPath() {
+        return serverHome + "/server/" + configuration + "/log/server.log";
+    }
 
-	protected void shutdownServer() throws IOException, InterruptedException {
-		Process shutdownProcess = Runtime.getRuntime().exec(
-                new String[]{serverHome + "/bin/shutdown.sh", "-s", "localhost:1"+portset+"99", "-S"});
-		shutdownProcess.waitFor();
-		if (shutdownProcess.exitValue() != 0) {
-			log.info("Failed to stop JBoss");
-		}
-	}
+    protected void shutdownServer() throws IOException, InterruptedException {
+        Process shutdownProcess = Runtime.getRuntime().exec(
+                new String[]{serverHome + createShutdownScript(), "-s", "localhost:1" + portset + "99", "-S"});
+        shutdownProcess.waitFor();
+        if (shutdownProcess.exitValue() != 0) {
+            log.info("Failed to stop JBoss");
+        }
+    }
 
-	private void deploy() throws Exception {
-		for (Deployment deployment : getDeployments()) {
-			deployment.deploy(getDeployDir());
-			waitFor(getTailProcess(), deployment.getWaitForMessage());
-		}
-		deploymentComplete = true;
+    private String createShutdownScript() {
+        if (winSystem()) {
+            return "/bin/shutdown.bat";
+        }
+        return "/bin/shutdown.sh";
+    }
+
+    private void deploy() throws Exception {
+        for (Deployment deployment : getDeployments()) {
+            deployment.deploy(getDeployDir());
+            waitFor(getTailProcess(), deployment.getWaitForMessage());
+        }
+        deploymentComplete = true;
 
         // close the tail process so it doesn't overload
         tailProcess.getInputStream().close();
-	}
+    }
 
-	private void undeploy() throws Exception {
-		for (Deployment deployment : getDeployments()) {
-			deployment.undeploy(getDeployDir());
-		}
-	}
+    private void undeploy() throws Exception {
+        for (Deployment deployment : getDeployments()) {
+            deployment.undeploy(getDeployDir());
+        }
+    }
 
-	public String getDeployDir() {
-		return serverHome + "/server/" + configuration + "/deploy/";
-	}
+    public String getDeployDir() {
+        return serverHome + "/server/" + configuration + "/deploy/";
+    }
 
-	protected static class SysoutLog {
-		public void info(String msg) {
-			System.out.println("--- " + msg);
-		}
-	}
+    protected static class SysoutLog {
+        public void info(String msg) {
+            System.out.println("--- " + msg);
+        }
+    }
 
-	private void scheduleTimeout() {
-		new Thread(
-			new Runnable() {
+    private void scheduleTimeout() {
+        new Thread(
+                new Runnable() {
 
-				@Override
-				public void run() {
-					try {
-						Thread.sleep(((long) getServerProperties().getDeploymentTimeoutMinutes()) * MILLISECONDS_IN_MINUTE);
-						if (!deploymentComplete) {
-							System.out.println("Timeout, shutting down JBoss");
-							shutdown();
-							throw new RuntimeException("Timeout");
-						}
-					} catch (InterruptedException e) {
-						// do nothing
-					} catch (Exception e) {
-						throw new RuntimeException(e);
-					}
-				}
-			}
-		).start();
-	}
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(((long) getServerProperties().getDeploymentTimeoutMinutes()) * MILLISECONDS_IN_MINUTE);
+                            if (!deploymentComplete) {
+                                System.out.println("Timeout, shutting down JBoss");
+                                shutdown();
+                                throw new RuntimeException("Timeout");
+                            }
+                        } catch (InterruptedException e) {
+                            // do nothing
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+        ).start();
+    }
 
-	protected void publishLog() throws IOException {
-		File tmpLogFile = File.createTempFile("jboss_log", ".txt");
-		Runtime.getRuntime().exec(new String[] {
-				"cp", getServerLogPath(), tmpLogFile.getAbsolutePath()
-		});
-		System.out.println("##teamcity[publishArtifacts '"+ tmpLogFile.getAbsolutePath()+"']");
-	}
+    protected void publishLog() throws IOException {
+        File tmpLogFile = File.createTempFile("jboss_log", ".txt");
+        Runtime.getRuntime().exec(new String[]{
+                "cp", getServerLogPath(), tmpLogFile.getAbsolutePath()
+        });
+        System.out.println("##teamcity[publishArtifacts '" + tmpLogFile.getAbsolutePath() + "']");
+    }
 }
