1b5b6dac5154b90684d06d287e2dc7e118838e0a
Pawel Wrzeszcz <pawel.wrzeszcz@gmail.com>
2010-10-20 21:31:17 +0200

    MockitoTestNGListener

---
+++ softwaremill-testing/test/java/pl/softwaremill/common/testing/MockitoTestNGListenerTest.java
@@ -0,0 +1,35 @@
+package pl.softwaremill.common.testing;
+
+import org.mockito.Mock;
+import org.mockito.internal.util.MockUtil;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * @author Pawel Wrzeszcz (pawel . wrzeszcz [at] gmail . com)
+ */
+@Listeners(MockitoTestNGListener.class)
+public class MockitoTestNGListenerTest {
+
+	@Mock
+	private Object mock;
+
+	@Test
+	public void shouldCreateMock() throws Exception {
+	    assertThat(new MockUtil().isMock(mock)).isTrue();
+	}
+
+	private Object mockFromPreviousMethod;		
+
+	@Test
+	public void shouldCreateNewMockForEachTestMethod_Part1() throws Exception {
+		mockFromPreviousMethod = mock;
+	}
+
+	@Test(dependsOnMethods = "shouldCreateNewMockForEachTestMethod_Part1")
+	public void shouldCreateNewMockForEachTestMethod_Part2() throws Exception {
+		assertThat(mockFromPreviousMethod).isNotEqualTo(mock);
+	}
+}
