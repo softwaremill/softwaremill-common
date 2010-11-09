d80aee8d569a1491c3ea5175d114dd898dc06090
Lukasz Lenart <lukasz@lenart.org.pl>
2010-11-09 10:27:26 +0100

    Implemented fluent interface and methods to operate on SuperClass

--- softwaremill-util/src/main/java/pl/softwaremill/common/util/RichObject.java
+++ softwaremill-util/src/main/java/pl/softwaremill/common/util/RichObject.java
@@ -12,11 +12,25 @@ public class RichObject {
         this.wrapped = wrapped;
     }
 
-    public void set(String propertyName, Object value) {
+    public RichObject set(String propertyName, Object value) {
         try {
             Field f = wrapped.getClass().getDeclaredField(propertyName);
             f.setAccessible(true);
             f.set(wrapped, value);
+            return this;
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public RichObject setSuper(String propertyName, Object value) {
+        try {
+            Field f = wrapped.getClass().getSuperclass().getDeclaredField(propertyName);
+            f.setAccessible(true);
+            f.set(wrapped, value);
+            return this;
         } catch (NoSuchFieldException e) {
             throw new RuntimeException(e);
         } catch (IllegalAccessException e) {
@@ -34,6 +48,22 @@ public class RichObject {
         } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public Object getSuper(String propertyName) {
+        try {
+            Field f = wrapped.getClass().getSuperclass().getDeclaredField(propertyName);
+            f.setAccessible(true);
+            return f.get(wrapped);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Object unwrap() {
+        return wrapped;
     }
 }
 
