aa1a6eff766ac1cbcee0c337f635f060f0dc1ec8
adamw <adam@warski.org>
2010-11-22 12:27:29 +0100

    A tool for emptying an SQS queue

---
+++ softwaremill-sqs/src/main/java/pl/softwaremill/common/sqs/SQSEmptor.java
@@ -0,0 +1,67 @@
+package pl.softwaremill.common.sqs;
+
+import com.xerox.amazonws.sqs2.Message;
+import com.xerox.amazonws.sqs2.MessageQueue;
+import com.xerox.amazonws.sqs2.SQSException;
+import com.xerox.amazonws.sqs2.SQSUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author Adam Warski (adam at warski dot org)
+ */
+public class SQSEmptor {
+    private final String accessKeyId;
+    private final String secretAccessKey;
+    private final String queueName;
+
+    public SQSEmptor(String accessKeyId, String secretAccessKey, String queueName) {
+        this.accessKeyId = accessKeyId;
+        this.secretAccessKey = secretAccessKey;
+        this.queueName = queueName;
+    }
+
+    private void emptyQueue() throws SQSException {
+        MessageQueue msgQueue = SQSUtils.connectToQueue(queueName, accessKeyId, secretAccessKey);
+
+        Message[] msgs;
+
+        do {
+            msgs = msgQueue.receiveMessages(10);
+            for (Message msg : msgs) {
+                System.out.println("Deleting message: " + msg.getMessageId());
+                msgQueue.deleteMessage(msg);
+            }
+        } while (msgs.length > 0);
+
+        System.out.println("Queue emptied");
+    }
+
+    public static void main(String[] args) throws IOException, SQSException {
+        BufferedReader rdr = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Access key id:");
+        String accessKeyId = rdr.readLine().trim();
+        if ("".equals(accessKeyId)) {
+            return;
+        }
+
+        System.out.println("Secret access key:");
+        String secretAccessKey = rdr.readLine().trim();
+        if ("".equals(secretAccessKey)) {
+            return;
+        }
+
+        while (true) {
+            System.out.println("Queue name to empty:");
+            String queueName = rdr.readLine().trim();
+            if ("".equals(queueName)) {
+                return;
+            }
+
+            new SQSEmptor(accessKeyId, secretAccessKey, queueName).emptyQueue();
+        }
+    }
+}
