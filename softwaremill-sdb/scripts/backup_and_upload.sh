1c94b67bd4fa1b47d4e01e22bfd75583c019ecee
adamw <adam@warski.org>
2010-12-07 14:37:54 +0100

    A backup script (backups domains, compresses them and uploads to S3), S3Upload class

---
+++ softwaremill-sdb/scripts/backup_and_upload.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Configuration
+###############
+
+# where the backups will be uploaded
+S3_ACCESS_KEY_ID=
+S3_SECRET_ACCESS_KEY=
+S3_BUCKET=
+
+# what to backup
+SIMPLEDB_ACCESS_KEY_ID=
+SIMPLEDB_SECRET_ACCESS_KEY=
+SIMPLEDB_REGION=US_EAST
+SIMPLEDB_DOMAINS=
+
+# path to the backup jar
+SDB_BACKUP_PATH=softwaremill-sdb-1-jar-with-dependencies.jar
+
+#########
+
+CURRENT_DATE=`date +"%d_%m_%Y"`
+CURRENT_DATE_WITH_HOUR=`date +"%d_%m_%Y__%H_%M"`
+TAR_FILE_NAME=simpledb_backup_${CURRENT_DATE_WITH_HOUR}.tar.gz
+
+# Creating backup files
+java -jar $SDB_BACKUP_PATH $SIMPLEDB_ACCESS_KEY_ID $SIMPLEDB_SECRET_ACCESS_KEY $SIMPLEDB_REGION $SIMPLEDB_DOMAINS
+
+# Packing
+tar -czf $TAR_FILE_NAME simpledb_backup*${CURRENT_DATE}*.txt
+
+# Removing backup files
+rm simpledb_backup*${CURRENT_DATE}*.txt
+
+# Uploading to S3
+java -cp $SDB_BACKUP_PATH pl.softwaremill.common.sdb.backup.S3Upload $S3_ACCESS_KEY_ID $S3_SECRET_ACCESS_KEY $S3_BUCKET $TAR_FILE_NAME
---
+++ softwaremill-sdb/src/main/java/pl/softwaremill/common/sdb/backup/S3Upload.java
@@ -0,0 +1,41 @@
+package pl.softwaremill.common.sdb.backup;
+
+import org.jets3t.service.S3ServiceException;
+import org.jets3t.service.impl.rest.httpclient.RestS3Service;
+import org.jets3t.service.model.S3Object;
+import org.jets3t.service.security.AWSCredentials;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author Adam Warski (adam at warski dot org)
+ */
+public class S3Upload {
+    private final String accessKeyId;
+    private final String secretAccessKey;
+    private final String bucketName;
+    private final String fileName;
+
+    public S3Upload(String accessKeyId, String secretAccessKey, String bucketName, String fileName) {
+        this.accessKeyId = accessKeyId;
+        this.secretAccessKey = secretAccessKey;
+        this.bucketName = bucketName;
+        this.fileName = fileName;
+    }
+
+    public void upload() throws S3ServiceException, IOException, NoSuchAlgorithmException {
+        RestS3Service s3 = new RestS3Service(new AWSCredentials(accessKeyId, secretAccessKey));
+        S3Object object = new S3Object(new File(fileName));
+        s3.putObject(bucketName, object);
+    }
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, S3ServiceException {
+        if (args.length != 4) {
+            System.out.println("Usage: S3Upload accessKeyId secretAccessKey bucketName fileName");
+        } else {
+            new S3Upload(args[0], args[1], args[2], args[3]).upload();
+        }
+    }
+}
