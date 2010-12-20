6066b995db4ca63332048627f6d28c36513d2d38
Lukasz Lenart <lukasz@lenart.org.pl>
2010-12-20 09:37:13 +0100

    Solves problem with redirecting all the time to Login page

--- softwaremill-faces/src/main/java/pl/softwaremill/common/faces/security/SecurityPhaseListener.java
+++ softwaremill-faces/src/main/java/pl/softwaremill/common/faces/security/SecurityPhaseListener.java
@@ -10,6 +10,7 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseEvent;
 import javax.faces.event.PhaseId;
 import javax.faces.event.PhaseListener;
+import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
@@ -32,85 +33,63 @@ public class SecurityPhaseListener implements PhaseListener {
 
     private final static String PREVIOUS_VIEW = "previous:viewId:redirect";
 
+    @Inject
+    private NavBase nav;
+
     public void beforePhase(PhaseEvent event) {
-        // check if we don't need to redirect
         FacesContext ctx = event.getFacesContext();
         HttpSession session = ((HttpSession) ctx.getExternalContext().getSession(true));
-
-        NavBase nav = BeanInject.lookup(NavBase.class);
         LoginBean login = BeanInject.lookup(LoginBean.class);
-
         Page page = nav.lookup(event.getFacesContext().getViewRoot().getViewId());
 
-        // if needed - handle redirects
-        if (nav.shouldRedirectToLogin()) {
-            // check redirect only if we're not on the login page
-            if (!page.equals(nav.getLogin()) &&
-                    // and redirect exists
-                    session.getAttribute(PREVIOUS_VIEW) != null) {
-
-                String redirect = (String) session.getAttribute(PREVIOUS_VIEW);
-
-                // set null so it doesn't re-redirect
-                session.setAttribute(PREVIOUS_VIEW, null);
-
-                // finally perform the redirect
-                try {
-                    ctx.getExternalContext().redirect(ctx.getExternalContext().encodeActionURL(redirect));
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-
-                ctx.responseComplete();
-                return;
+        if (page.isRequiresLogin() && !login.isLoggedIn()) {
+            if (nav.shouldRedirectToLogin()) {
+                redirectToLogin(ctx, session, nav, page);
+            } else {
+                responseForbidden(ctx);
             }
-
-            if (page.isRequiresLogin() && !login.isLoggedIn()) {
-                String url = ctx.getApplication().getViewHandler().getActionURL(ctx, nav.getLogin().s());
-
-                // remember this view to redirect back to it
-                session.setAttribute(PREVIOUS_VIEW, ctx.getApplication().getViewHandler().getActionURL(ctx, page.s()));
-
-                try {
-
-                    ctx.getExternalContext().redirect(ctx.getExternalContext().encodeActionURL(url));
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                ctx.responseComplete();
-
-                return;
-            }
+        } else if (login.isLoggedIn() && session.getAttribute(PREVIOUS_VIEW) != null) {
+            redirectToPreviousView(ctx, session);
+        } else if (page.getSecurityEL() != null && page.getSecurityEL().length() > 0) {
+            evaluateSecurityExpression(ctx, page);
         }
-        else {
-            // otherwise check isLoggerIn()
-            if (page.isRequiresLogin() && !login.isLoggedIn()) {
-                // show 403 Forbidden - user not logged in and do not know what to do with it
-                ctx.getExternalContext().setResponseStatus(403);
+    }
 
-                ctx.responseComplete();
-            }
+    private void redirectToLogin(FacesContext ctx, HttpSession session, NavBase nav, Page page) {
+        session.setAttribute(PREVIOUS_VIEW, ctx.getApplication().getViewHandler().getActionURL(ctx, page.s()));
+        try {
+            String url = ctx.getApplication().getViewHandler().getActionURL(ctx, nav.getLogin().s());
+            ctx.getExternalContext().redirect(ctx.getExternalContext().encodeActionURL(url));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+        ctx.responseComplete();
+    }
 
+    private void redirectToPreviousView(FacesContext ctx, HttpSession session) {
+        String redirect = (String) session.getAttribute(PREVIOUS_VIEW);
+        session.setAttribute(PREVIOUS_VIEW, null);
+        try {
+            ctx.getExternalContext().redirect(ctx.getExternalContext().encodeActionURL(redirect));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        ctx.responseComplete();
+    }
 
-        // otherwise check the security el
+    private void responseForbidden(FacesContext ctx) {
+        ctx.getExternalContext().setResponseStatus(403);
+        ctx.responseComplete();
+    }
 
-        if (page.getSecurityEL() != null && page.getSecurityEL().length() > 0) {
-            ELEvaluator evaluator = BeanInject.lookup(ELEvaluator.class);
-
-            Boolean securityResult = evaluator.evaluate(page.getSecurityEL(), Boolean.class);
-
-            if (securityResult == null) {
-                throw new RuntimeException("Security EL: " + page.getSecurityEL() +
-                        "on page " + page.s() + " doesn't resolve to Boolean");
-            }
-
-            if (!securityResult) {
-                // show 403 Forbidden - the security EL resolved to false
-                ctx.getExternalContext().setResponseStatus(403);
-
-                ctx.responseComplete();
-            }
+    private void evaluateSecurityExpression(FacesContext ctx, Page page) {
+        ELEvaluator evaluator = BeanInject.lookup(ELEvaluator.class);
+        Boolean securityResult = evaluator.evaluate(page.getSecurityEL(), Boolean.class);
+        if (securityResult == null) {
+            throw new RuntimeException("Security EL: " + page.getSecurityEL() + "on page " + page.s() + " doesn't resolve to Boolean");
+        }
+        if (!securityResult) {
+            responseForbidden(ctx);
         }
     }
 
