0c318a5a15c08d88f6274a387c524f623f42b1d0
Lukasz Lenart <lukasz@lenart.org.pl>
2010-12-22 14:36:42 +0100

    Solves NPE when used outside Managed Bean

--- softwaremill-cdi/src/main/java/pl/softwaremill/common/cdi/logger/LoggerProducer.java
+++ softwaremill-cdi/src/main/java/pl/softwaremill/common/cdi/logger/LoggerProducer.java
@@ -4,14 +4,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.InjectionPoint;
 
 /**
  * @author Adam Warski (adam at warski dot org)
  */
 public class LoggerProducer {
+
     @Produces
     public Logger produceLog(InjectionPoint injectionPoint) {
-        return LoggerFactory.getLogger(injectionPoint.getBean().getBeanClass());
+        Bean<?> bean = injectionPoint.getBean();
+        if (bean == null) {
+            return LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass());
+        } else {
+            return LoggerFactory.getLogger(bean.getBeanClass());
+        }
     }
+
 }
