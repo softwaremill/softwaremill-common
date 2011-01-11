bc775e4f9d241de688fd40a0e978d8313b61ba79
adamw <adam@warski.org>
2011-01-11 13:53:34 +0100

    Removing unneeded variables

--- softwaremill-backup/scripts/restore_sdb.sh
+++ softwaremill-backup/scripts/restore_sdb.sh
@@ -13,9 +13,5 @@ source backup_conf.sh
 
 #########
 
-CURRENT_DATE=`date +"%d_%m_%Y"`
-CURRENT_DATE_WITH_HOUR=`date +"%d_%m_%Y__%H_%M"`
-TAR_FILE_NAME=simpledb_backup_${CURRENT_DATE_WITH_HOUR}.tar.gz
-
 # Creating backup files
 java -cp $SDB_BACKUP_PATH pl.softwaremill.common.backup.RunRestore $SIMPLEDB_ACCESS_KEY_ID $SIMPLEDB_SECRET_ACCESS_KEY $SIMPLEDB_REGION $1 $2
