16640172070f48cf0c42a0e360a351d6dbe67c4f
Adam Warski <adam@warski.org>
2011-07-08 12:13:25 +0200

    Adding protocol to AS7 deploy props provider

--- softwaremill-conf/src/main/java/pl/softwaremill/common/conf/JBoss7DeployPropertiesProvider.java
+++ softwaremill-conf/src/main/java/pl/softwaremill/common/conf/JBoss7DeployPropertiesProvider.java
@@ -1,19 +1,14 @@
 package pl.softwaremill.common.conf;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * JBoss 7 configuration provider
  */
 public class JBoss7DeployPropertiesProvider extends FolderPropertiesProvider {
-
-    private static final Logger log = LoggerFactory.getLogger(JBoss7DeployPropertiesProvider.class);
     public static final String JBOSS_CONFIG_DIR_PROPERTY = "jboss.server.config.dir";
 
     @Override
     public String lookupConfigDirectory() {
-        return System.getProperty(JBOSS_CONFIG_DIR_PROPERTY);
+        return "file:///" + System.getProperty(JBOSS_CONFIG_DIR_PROPERTY);
     }
 
     @Override
