8c78a2ec6367152d477f7aa4c729f2a51a5a9309
lukaszlenart <lukasz.lenart@gmail.com>
2011-09-26 15:59:57 +0200

    #48 Adds new AbstractWebServiceTest to simply use of soapUI test suites

---
+++ softwaremill-test/softwaremill-test-ui-web/src/main/java/pl/softwaremill/common/test/web/soap/AbstractWebServiceTest.java
@@ -0,0 +1,78 @@
+package pl.softwaremill.common.test.web.soap;
+
+import com.eviware.soapui.impl.wsdl.WsdlProject;
+import com.eviware.soapui.model.support.PropertiesMap;
+import com.eviware.soapui.model.testsuite.TestCase;
+import com.eviware.soapui.model.testsuite.TestRunner;
+import com.eviware.soapui.model.testsuite.TestSuite;
+
+import java.util.List;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * Base class used to create web service test base on soapUI Test Suites
+ */
+public class AbstractWebServiceTest {
+
+    private ArtifactPublisherListener listener;
+
+    public AbstractWebServiceTest() {
+        listener = new ArtifactPublisherListener();
+    }
+
+    /**
+     * Runs all the tests defined in the given soapUI Test Suite,
+     *
+     * @param soapUITestSuite specify it like for example: System.getProperty("resources.dir") + "/src/test/resources/SoapUITestSuite.xml"
+     * @throws Exception any exception
+     */
+    protected void run(String soapUITestSuite) throws Exception {
+        run(new WsdlProject(soapUITestSuite));
+    }
+
+    /**
+     * Runs SoapUI TestCase project
+     *
+     * @param project new WsdlProject("/path/to/soapui/testcase.xml")
+     */
+    protected void run(WsdlProject project) {
+        for (TestSuite testSuite : project.getTestSuiteList()) {
+            run(testSuite);
+        }
+    }
+
+    /**
+     * Runs all test cases in given SoapUI TestSuite
+     *
+     * @param testSuite SoapUI TestSuite
+     */
+    protected void run(TestSuite testSuite) {
+        run(testSuite.getTestCaseList());
+    }
+
+    /**
+     * Runs specified list of TestCases one by one
+     *
+     * @param testCases list of TestCases
+     */
+    protected void run(List<TestCase> testCases) {
+        for (TestCase testCase : testCases) {
+            run(testCase);
+        }
+    }
+
+    /**
+     * Runs one soapUI TestCase
+     *
+     * @param testCase single TestCase
+     */
+    protected void run(TestCase testCase) {
+        testCase.addTestRunListener(listener);
+        TestRunner runner = testCase.run(new PropertiesMap(), false);
+        assertThat(runner.getStatus())
+                .describedAs("TestCase [" + testCase.getLabel() + "] failed because of [" + runner.getReason() + "]")
+                .isEqualTo(TestRunner.Status.FINISHED);
+    }
+
+}
---
+++ softwaremill-test/softwaremill-test-ui-web/src/main/java/pl/softwaremill/common/test/web/soap/ArtifactPublisherListener.java
@@ -0,0 +1,53 @@
+package pl.softwaremill.common.test.web.soap;
+
+import com.eviware.soapui.model.testsuite.TestCaseRunContext;
+import com.eviware.soapui.model.testsuite.TestCaseRunner;
+import com.eviware.soapui.model.testsuite.TestRunListener;
+import com.eviware.soapui.model.testsuite.TestRunner;
+import com.eviware.soapui.model.testsuite.TestStep;
+import com.eviware.soapui.model.testsuite.TestStepResult;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Publish result of Failed soapUI test case as a TeamCity artifact
+ */
+public class ArtifactPublisherListener implements TestRunListener {
+
+    @Override
+    public void beforeRun(TestCaseRunner testCaseRunner, TestCaseRunContext testCaseRunContext) {
+    }
+
+    @Override
+    public void afterRun(TestCaseRunner testCaseRunner, TestCaseRunContext testCaseRunContext) {
+    }
+
+    @Override
+    public void beforeStep(TestCaseRunner testCaseRunner, TestCaseRunContext testCaseRunContext) {
+    }
+
+    @Override
+    public void beforeStep(TestCaseRunner testCaseRunner, TestCaseRunContext testCaseRunContext, TestStep testStep) {
+    }
+
+    @Override
+    public void afterStep(TestCaseRunner testCaseRunner, TestCaseRunContext testCaseRunContext, TestStepResult testStepResult) {
+        try {
+            if (TestRunner.Status.FAILED.equals(testCaseRunner.getStatus())) {
+                String date = new SimpleDateFormat(".yyyy-MM-dd-HH-mm-ss.").format(new Date());
+                File f = File.createTempFile("sopaui_result_" + testStepResult.getTestStep().getLabel() + "_", date + ".txt");
+                FileOutputStream fos = new FileOutputStream(f);
+                PrintWriter pw = new PrintWriter(fos);
+                testStepResult.writeTo(pw);
+                pw.close();
+                fos.close();
+                System.out.println("##teamcity[publishArtifacts '" + f.getAbsolutePath() + "']");
+            }
+        } catch (Exception ignore) {
+        }
+    }
+}
