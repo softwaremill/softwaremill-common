886b09c57128ff6558640a08771bb0f017cf6ce4
lukaszlenart <lukasz.lenart@gmail.com>
2011-09-27 12:16:37 +0200

    #48 Extends adapter to simplify implementation and uses getError() to check if test step fail

--- softwaremill-test/softwaremill-test-ui-web/src/main/java/pl/softwaremill/common/test/web/soap/ArtifactPublisherListener.java
+++ softwaremill-test/softwaremill-test-ui-web/src/main/java/pl/softwaremill/common/test/web/soap/ArtifactPublisherListener.java
@@ -1,9 +1,8 @@
 package pl.softwaremill.common.test.web.soap;
 
+import com.eviware.soapui.model.support.TestRunListenerAdapter;
 import com.eviware.soapui.model.testsuite.TestCaseRunContext;
 import com.eviware.soapui.model.testsuite.TestCaseRunner;
-import com.eviware.soapui.model.testsuite.TestRunListener;
-import com.eviware.soapui.model.testsuite.TestStep;
 import com.eviware.soapui.model.testsuite.TestStepResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,33 +16,19 @@ import java.util.Date;
 /**
  * Publish result of Failed soapUI test case as a TeamCity artifact
  */
-public class ArtifactPublisherListener implements TestRunListener {
+public class ArtifactPublisherListener extends TestRunListenerAdapter {
 
     private final static Logger LOG = LoggerFactory.getLogger(ArtifactPublisherListener.class);
-
-    @Override
-    public void beforeRun(TestCaseRunner testCaseRunner, TestCaseRunContext testCaseRunContext) {
-    }
-
-    @Override
-    public void afterRun(TestCaseRunner testCaseRunner, TestCaseRunContext testCaseRunContext) {
-    }
-
-    @Override
-    public void beforeStep(TestCaseRunner testCaseRunner, TestCaseRunContext testCaseRunContext) {
-    }
-
-    @Override
-    public void beforeStep(TestCaseRunner testCaseRunner, TestCaseRunContext testCaseRunContext, TestStep testStep) {
-    }
 
     @Override
     public void afterStep(TestCaseRunner testCaseRunner, TestCaseRunContext testCaseRunContext, TestStepResult testStepResult) {
         try {
             LOG.info("soapUI TestCase:TestStep [" + testCaseRunner.getTestCase().getLabel() + ":" +
-                    testStepResult.getTestStep().getLabel() +
-                    "] finished with status [" + testStepResult.getStatus() + "]");
-            if (TestStepResult.TestStepStatus.FAILED.equals(testStepResult.getStatus())) {
+                    testStepResult.getTestStep().getLabel() + "] finished with status [" + testStepResult.getStatus() + "]");
+            Throwable error = testStepResult.getError();
+            if (error != null) {
+                LOG.error("Error occurred during running TestCase:TestStep [" + testCaseRunner.getTestCase().getLabel() + ":" +
+                        testStepResult.getTestStep().getLabel() + "]", error);
                 String date = new SimpleDateFormat(".yyyy-MM-dd-HH-mm-ss.").format(new Date());
                 File f = File.createTempFile("sopaui_result_" + testStepResult.getTestStep().getLabel() + "_", date + ".txt");
                 FileOutputStream fos = new FileOutputStream(f);
@@ -57,4 +42,5 @@ public class ArtifactPublisherListener implements TestRunListener {
             LOG.error("Error occurred during publishing artifact!", e);
         }
     }
+
 }
