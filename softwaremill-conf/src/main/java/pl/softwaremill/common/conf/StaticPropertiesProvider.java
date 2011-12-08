c9a53aa8ee104b4c0b8d1d03f538a3f20502ddb1
Adam Warski <adam@warski.org>
2011-12-08 10:07:36 +0100

    The map must be not-null ;)

--- softwaremill-conf/src/main/java/pl/softwaremill/common/conf/StaticPropertiesProvider.java
+++ softwaremill-conf/src/main/java/pl/softwaremill/common/conf/StaticPropertiesProvider.java
@@ -2,13 +2,15 @@ package pl.softwaremill.common.conf;
 
 import com.google.common.collect.ImmutableMap;
 
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 /**
  * @author Adam Warski (adam at warski dot org)
  */
 public class StaticPropertiesProvider implements PropertiesProvider {
-    private static ConcurrentMap<String, ImmutableMap<String, String>> configurations;
+    private static ConcurrentMap<String, ImmutableMap<String, String>> configurations =
+            new ConcurrentHashMap<String, ImmutableMap<String, String>>();
     
     @Override
     public ImmutableMap<String, String> lookupProperties(String name) {
