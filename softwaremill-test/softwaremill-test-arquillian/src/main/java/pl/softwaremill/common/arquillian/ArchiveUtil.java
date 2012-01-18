1fe68f5bdc8f1d22a8af16d5ad31a6b15af1ce24
pawel <pawel@sofwaremill.pl>
2012-01-18 15:24:30 +0100

    #69 Shorthand for creating archive in Arquillian

---
+++ softwaremill-test/softwaremill-test-arquillian/src/main/java/pl/softwaremill/common/arquillian/ArchiveUtil.java
@@ -0,0 +1,14 @@
+package pl.softwaremill.common.arquillian;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+
+/**
+ * @author Pawel Wrzeszcz (pawel [at] softwaremill . com)
+ */
+public class ArchiveUtil {
+
+	public static JavaArchive createArchive() {
+		return ShrinkWrap.create(JavaArchive.class, "test.jar");
+	}
+}
