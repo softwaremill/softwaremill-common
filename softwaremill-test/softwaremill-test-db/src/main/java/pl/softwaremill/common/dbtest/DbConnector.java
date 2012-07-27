b67e097067c883ab827fb33c556cc00bd90e1cd0
Pawel Stawicki <pawelstawicki@gmail.com>
2012-07-27 14:11:49 +0200

    Added DbConnector. Useful when we want to have control over transactions in tests.
    I'd like to use it in AbstractDBTest too. Now there is some duplicated code.

---
+++ softwaremill-test/softwaremill-test-db/src/main/java/pl/softwaremill/common/dbtest/DbConnector.java
@@ -0,0 +1,61 @@
+package pl.softwaremill.common.dbtest;
+
+import org.hibernate.cfg.Environment;
+import org.hibernate.ejb.AvailableSettings;
+import org.hibernate.ejb.Ejb3Configuration;
+import org.hibernate.testing.tm.ConnectionProviderImpl;
+import org.hibernate.testing.tm.TransactionManagerLookupImpl;
+import pl.softwaremill.common.dbtest.util.DbMode;
+
+import javax.persistence.EntityManagerFactory;
+
+/**
+ * @author Pawel Stawicki
+ * @since 7/27/12 1:15 PM
+ */
+public class DbConnector {
+
+    private Ejb3Configuration configuration;
+
+    public DbConnector() {
+        this("hibernate.test.cfg.xml");
+    }
+
+    public DbConnector(String hibernateConfigurationFileResource) {
+        createEjb3Configuration(hibernateConfigurationFileResource);
+    }
+
+    private void createEjb3Configuration(String hibernateConfigurationFileResource) {
+        configuration = new Ejb3Configuration();
+
+        configuration.configure(hibernateConfigurationFileResource);
+
+        // Separate database for each test class
+        configuration.setProperty("hibernate.connection.url", "jdbc:h2:mem:" + this.getClass().getName());
+
+        configuration.setProperty("connection.provider_class", ConnectionProviderImpl.class.getName());
+        configuration.setProperty(Environment.TRANSACTION_MANAGER_STRATEGY, TransactionManagerLookupImpl.class.getName());
+        configuration.setProperty(AvailableSettings.TRANSACTION_TYPE, "RESOURCE_LOCAL");
+    }
+
+    public void setCompatibilityMode(DbMode compatibilityMode) {
+        configuration.setProperty("hibernate.connection.url", "jdbc:h2:mem:" + this.getClass().getName() + addCompatibilityMode(compatibilityMode));
+    }
+
+    private String addCompatibilityMode(DbMode compatibilityMode) {
+        if (compatibilityMode != null) {
+            return ";MODE=" + compatibilityMode.getParameterValue();
+        }
+
+        return "";
+    }
+
+    public Ejb3Configuration getConfiguration() {
+        return configuration;
+    }
+
+    public EntityManagerFactory createEntityManagerFactory() {
+        return configuration.buildEntityManagerFactory();
+    }
+
+}
---
+++ softwaremill-test/softwaremill-test-db/src/test/java/pl/softwaremill/common/dbtest/DbConnectorTest.java
@@ -0,0 +1,79 @@
+package pl.softwaremill.common.dbtest;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pl.softwaremill.common.dbtest.util.DbMode;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * @author Pawel Stawicki
+ * @since 7/27/12 1:59 PM
+ */
+public class DbConnectorTest {
+
+    private static final long ID = 1L;
+    private static final long ID2 = 2L;
+
+    private EntityManagerFactory emf;
+
+    @BeforeClass
+    private void createEntityManagerFactoryFromDbConnector() {
+        DbConnector connector = new DbConnector();
+
+        connector.getConfiguration().addAnnotatedClass(TestEntity1.class);
+        connector.setCompatibilityMode(DbMode.DB2);
+
+        emf = connector.createEntityManagerFactory();
+    }
+
+    @Test
+    public void shouldNotSeeChangesInUncommitedTransactionFromAnotherTransaction() {
+        // Given
+        EntityManager em1 = emf.createEntityManager();
+        EntityManager em2 = emf.createEntityManager();
+
+        // When
+        em1.getTransaction().begin();
+        TestEntity1 entity = new TestEntity1();
+        entity.setId(ID);
+        em1.persist(entity);
+
+        em2.getTransaction().begin();
+        TestEntity1 entityFromSecondTransaction = em2.find(TestEntity1.class, ID);
+
+        // Then
+        assertThat(entityFromSecondTransaction).isNull();
+
+        // Cleanup
+        em1.getTransaction().rollback();
+        em2.getTransaction().rollback();
+    }
+
+    @Test
+    public void shouldSeeChangesInCommitedTransactionFromAnotherTransaction() {
+        // Given
+        EntityManager em1 = emf.createEntityManager();
+        EntityManager em2 = emf.createEntityManager();
+
+        // When
+        em1.getTransaction().begin();
+        TestEntity1 entity = new TestEntity1();
+        entity.setId(ID2);
+        em1.persist(entity);
+        em1.getTransaction().commit();
+
+        em2.getTransaction().begin();
+        TestEntity1 entityFromSecondTransaction = em2.find(TestEntity1.class, ID2);
+
+        // Then
+        assertThat(entityFromSecondTransaction).isNotNull();
+
+        // Cleanup
+        em2.getTransaction().rollback();
+    }
+
+}
