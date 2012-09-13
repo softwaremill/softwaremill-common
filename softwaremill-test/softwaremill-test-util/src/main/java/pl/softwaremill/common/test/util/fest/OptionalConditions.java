0ab949ec81469d85ef94d90cc29315a8f1ce55d9
Maciej Bilas <maciej.bilas@gmail.com>
2012-09-13 12:20:59 +0200

    Adds OptionalConditions for cleaner assertions on Guava's Optional<T>.

---
+++ softwaremill-test/softwaremill-test-util/src/main/java/pl/softwaremill/common/test/util/fest/OptionalConditions.java
@@ -0,0 +1,47 @@
+package pl.softwaremill.common.test.util.fest;
+
+import com.google.common.base.Optional;
+import org.fest.assertions.Condition;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+/**
+ * @author Maciej Bilas
+ * @since 13/9/12 12:07
+ */
+public class OptionalConditions {
+
+    private static final Condition<Object> PRESENT_CONDITION = new Condition<Object>() {
+        @SuppressWarnings("ConstantConditions")
+        @Override
+        public boolean matches(Object o) {
+            if (o == null)
+                return false;
+
+            checkArgument(o instanceof Optional);
+
+            return ((Optional<?>) o).isPresent();
+        }
+    };
+
+    private static final Condition<Object> ABSENT_CONDITION = new Condition<Object>() {
+        @SuppressWarnings("ConstantConditions")
+        @Override
+        public boolean matches(Object o) {
+            if (o == null)
+                return false;
+
+            checkArgument(o instanceof Optional);
+
+            return !((Optional) o).isPresent();
+        }
+    };
+
+    public static Condition<Object> present() {
+        return PRESENT_CONDITION;
+    }
+
+    public static Condition<Object> absent() {
+        return ABSENT_CONDITION;
+    }
+}
---
+++ softwaremill-test/softwaremill-test-util/src/test/java/pl/softwaremill/common/test/util/fest/OptionalConditionsTest.java
@@ -0,0 +1,48 @@
+package pl.softwaremill.common.test.util.fest;
+
+import com.google.common.base.Optional;
+import org.testng.annotations.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static pl.softwaremill.common.test.util.fest.OptionalConditions.absent;
+import static pl.softwaremill.common.test.util.fest.OptionalConditions.present;
+
+/**
+ * @author Maciej Bilas
+ * @since 13/9/12 12:10
+ */
+public class OptionalConditionsTest {
+
+    @Test
+    public void shouldMatchAbsentOptionals() {
+        assertThat(Optional.absent()).is(absent());
+        assertThat(Optional.absent()).isNot(present());
+    }
+
+    @Test
+    public void shouldMatchPresentOptionals() {
+        assertThat(Optional.of("foo")).is(present());
+        assertThat(Optional.of("foo")).isNot(absent());
+    }
+
+    @Test
+    public void presentConditionShouldBeFalseOnNullObjects() {
+        assertThat(present().matches(null)).isFalse();
+    }
+
+    @Test
+    public void absentConditionShouldBeFalseOnNullObjects() {
+        assertThat(absent().matches(null)).isFalse();
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void presentConditionShouldFailOnObjectsThatAreNotOptionals() {
+        present().matches("foo");
+    }
+
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void absentConditionShouldFailOnObjectsThatAreNotOptionals() {
+        absent().matches("foo");
+    }
+}
