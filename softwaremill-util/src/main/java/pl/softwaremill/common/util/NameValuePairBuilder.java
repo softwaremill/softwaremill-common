929b836085fb5172dbe44be68da64774d3b6ea6b
Tomasz Dziurko <tdziurko@gmail.com>
2013-02-26 10:20:52 +0100

    Added NameValuePairBuilder to make building NameValuePairs easier, that's it :)

---
+++ softwaremill-util/src/main/java/pl/softwaremill/common/util/NameValuePairBuilder.java
@@ -0,0 +1,34 @@
+package pl.softwaremill.common.util;
+
+import com.google.common.collect.Maps;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.Map;
+
+public class NameValuePairBuilder {
+
+    public static final String NOT_UNIQUE_KEY_ERROR = "Map already contains key with name ";
+
+    private Map<String, String> nameValueMap = Maps.newHashMap();
+
+    public NameValuePairBuilder addPair(String name, String value) {
+        if(nameValueMap.containsKey(name))  {
+            throw new RuntimeException(NOT_UNIQUE_KEY_ERROR + name);
+        }
+        nameValueMap.put(name, value);
+        return this;
+    }
+
+    public NameValuePair[] build() {
+
+        NameValuePair[] array = new NameValuePair[nameValueMap.keySet().size()];
+
+        int index = 0;
+        for (String name : nameValueMap.keySet()) {
+            String value = nameValueMap.get(name);
+            array[index++]= new BasicNameValuePair(name, value);
+        }
+        return array;
+    }
+}
---
+++ softwaremill-util/src/test/java/pl/softwaremill/common/util/NameValuePairBuilderTest.java
@@ -0,0 +1,46 @@
+package pl.softwaremill.common.util;
+
+import org.apache.http.NameValuePair;
+import org.testng.annotations.Test;
+
+import static com.googlecode.catchexception.CatchException.catchException;
+import static com.googlecode.catchexception.CatchException.caughtException;
+import static org.fest.assertions.Assertions.assertThat;
+
+public class NameValuePairBuilderTest {
+
+    @Test
+    public void shouldNotAllowDuplicateNames() {
+        // Given
+        NameValuePairBuilder builder = new NameValuePairBuilder();
+        builder.addPair("unique", "value");
+
+        // When
+        catchException(builder).addPair("unique", "newValue");
+
+        // Then
+        assertThat(caughtException()).isExactlyInstanceOf(RuntimeException.class);
+        assertThat(caughtException()).hasMessage(NameValuePairBuilder.NOT_UNIQUE_KEY_ERROR + "unique");
+    }
+
+    @Test
+    public void shouldCreatePairsArray() {
+        // Given
+        NameValuePairBuilder builder = new NameValuePairBuilder();
+
+        // When
+        builder
+                .addPair("name1", "value1")
+                .addPair("name2", "value2");
+        NameValuePair[] nameValuePairs = builder.build();
+
+        // Then
+        assertThat(nameValuePairs).hasSize(2);
+
+        assertThat(nameValuePairs[0].getName()).isEqualTo("name1");
+        assertThat(nameValuePairs[0].getValue()).isEqualTo("value1");
+
+        assertThat(nameValuePairs[1].getName()).isEqualTo("name2");
+        assertThat(nameValuePairs[1].getValue()).isEqualTo("value2");
+    }
+}
