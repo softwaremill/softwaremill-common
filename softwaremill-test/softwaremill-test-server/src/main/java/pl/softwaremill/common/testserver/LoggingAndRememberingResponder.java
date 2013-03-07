9fc86ddd124db7be32f5046b030bbf2896ffe9c5
Paweł Stawicki <pawelstawicki@gmail.com>
2013-03-07 22:10:52 +0100

    [minor] @NotThreadSafe annotation added

--- softwaremill-test/softwaremill-test-server/src/main/java/pl/softwaremill/common/testserver/LoggingAndRememberingResponder.java
+++ softwaremill-test/softwaremill-test-server/src/main/java/pl/softwaremill/common/testserver/LoggingAndRememberingResponder.java
@@ -6,6 +6,7 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.concurrent.NotThreadSafe;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -15,6 +16,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@NotThreadSafe
 public class LoggingAndRememberingResponder implements Responder {
 
     private static final Logger log = LoggerFactory.getLogger(LoggingAndRememberingResponder.class);
